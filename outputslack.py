cea5af9fb362e12e125735aef2b42575c41c5aa3
fariddzenetov <fariddjenetov75@gmail.com>
2025-04-07 23:18:51 +0300

    v2

--- outputslack.py
+++ outputslack.py
@@ -2,6 +2,5 @@ import requests
 import json
 
 
-
-def send_slack_message(payload,webhook):
-    return requests.post(webhook,json.dumps(payload))
+def send_slack_message(payload, webhook):
+    return requests.post(webhook, json.dumps(payload))
